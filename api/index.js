8f166c71ff50ffd87f482f3d06aea88ba383aef9
Fer <Fermincho.zt7@gmail.com>
2022-07-18 17:45:40 -0300

    modelo sequelize de pais hecho y cambios menores para que el back levante correctamente

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const server = require('./src/server.js');
-const { conn } = require('./src/db.js');
+const { db } = require('./src/db.js');
 
-conn.sync({ force: true }).then(() => {
+db.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log('cuchando'); 
   });
