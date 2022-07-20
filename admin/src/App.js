794d755ab2c0e0a270c57da1b30131ddc4934131
Andres <afguerreroleal12@gmail.com>
2022-07-20 09:37:10 -0500

    Se añade configuración de tailwind

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,22 +1,9 @@
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="text-3xl font-bold underline text-gray-400">
+      Admin
+  </h1>
   );
 }
 
