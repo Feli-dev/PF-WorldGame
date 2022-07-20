0ec4ef53032071ba60c1c0ca225d88a9750d7a31
Andres <afguerreroleal12@gmail.com>
2022-07-20 09:51:22 -0500

    Se añade react-router-dom y rutas principales

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import NoFound from "./pages/404/NoFound";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-gray-400">
-      Admin
-  </h1>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/*" component={NoFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
