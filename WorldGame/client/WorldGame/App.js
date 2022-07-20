141ddfd0c591b10609092478329eee80a987f7a0
Fer <Fermincho.zt7@gmail.com>
2022-07-20 15:56:06 -0300

    página de instrucciones, falta dar uso a los botones

--- WorldGame/client/WorldGame/App.js
+++ WorldGame/client/WorldGame/App.js
@@ -1,12 +1,14 @@
 
 import { Text, View, TouchableOpacity, TextInput } from 'react-native';
-import tw from "twrnc"
+import tw from "twrnc";
+import Register from "./views/Register";
+import Instructions from './views/Instructions';
 
 
 export default function App() {
   return (
     <View style={tw`flex h-full items-center justify-center bg-gray-900`}>
-      <Register/>
+      <Instructions />
     </View>
   );
 }
