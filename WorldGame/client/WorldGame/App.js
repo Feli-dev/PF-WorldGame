cfa5288af1cea405825616cce9578c3b95f152a5
rafacarreno <89260514+rafacarreno@users.noreply.github.com>
2022-07-21 10:33:21 -0300

    update App

--- WorldGame/client/WorldGame/App.js
+++ WorldGame/client/WorldGame/App.js
@@ -1,12 +1,15 @@
 
 import { Text, View, TouchableOpacity, TextInput } from 'react-native';
 import tw from "twrnc"
+import Register from './views/Register'
+import Info from './views/Info';
 
 
 export default function App() {
   return (
     <View style={tw`flex h-full items-center justify-center bg-gray-900`}>
       <Register/>
+      <Info/>
     </View>
   );
 }
