9ae688c790e3c0e47c75a06f7a110ea9cbed9808
MamelCas <109260435+MamelCas@users.noreply.github.com>
2022-07-20 22:22:49 -0500

    Configuracion para deploy

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -6,10 +6,12 @@ const user = require('./user.js');
 const login = require('./login.js');
 const games = require('./games.js');
 const friends = require('./friends.js');
+const countries = require('./countries');
 
 router.use('/User', user);
 router.use('/Login', login);
 router.use('/friends', friends);
 router.use('/games', games);
+router.use('/countries', countries)
 
 module.exports = router;
