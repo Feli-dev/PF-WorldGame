366626483d5fd95a5edcb736847506cf92227da6
Felipe Tau <felipetau0@gmail.com>
2022-07-27 00:53:03 -0300

    show ad - not function to show when winning game

--- client/views/Configuration.jsx
+++ client/views/Configuration.jsx
@@ -1,10 +1,20 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Platform, Button } from "react-native";
+import React, { useEffect } from "react";
+import tw from "twrnc";
+import { setTestDeviceIDAsync, AdMobInterstitial } from "expo-ads-admob";
 
 export default function Configuration() {
+  async function chargeAds(){
+    await setTestDeviceIDAsync('EMULATOR');
+    await AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/8691691433'); // Test ID, Replace with your-admob-unit-id
+    await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true});
+    await AdMobInterstitial.showAdAsync();
+  }
+  chargeAds()  
+    
   return (
-    <View>
-      <Text>Configuration</Text>
+    <View style={tw`h-full bg-gray-900 flex justify-center items-center`}>
+      
     </View>
   );
 }
