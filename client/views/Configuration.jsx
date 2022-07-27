e1bec4a2d59116c913ecfc1d4bb7108fdf99bfe2
Felipe Tau <felipetau0@gmail.com>
2022-07-27 15:05:00 -0300

    v1.0.0 ads on game

--- client/views/Configuration.jsx
+++ client/views/Configuration.jsx
@@ -1,17 +1,8 @@
 import { View, Text, Platform, Button } from "react-native";
 import React, { useEffect } from "react";
 import tw from "twrnc";
-import { setTestDeviceIDAsync, AdMobInterstitial } from "expo-ads-admob";
 
 export default function Configuration() {
-  async function chargeAds(){
-    await setTestDeviceIDAsync('EMULATOR');
-    await AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/8691691433'); // Test ID, Replace with your-admob-unit-id
-    await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true});
-    await AdMobInterstitial.showAdAsync();
-  }
-  chargeAds()  
-    
   return (
     <View style={tw`h-full bg-gray-900 flex justify-center items-center`}>
       
