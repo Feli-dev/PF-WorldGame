58f81b755889a3bd17f30f6f51e16e2645f64b81
Felipe Tau <felipetau0@gmail.com>
2022-07-27 15:45:29 -0300

    v1.0

--- client/views/Configuration.jsx
+++ client/views/Configuration.jsx
@@ -1,20 +1,22 @@
-import { View, Text, Platform, Button } from "react-native";
+import { View, Text, Platform, TouchableOpacity } from "react-native";
 import React, { useEffect } from "react";
 import tw from "twrnc";
-import { setTestDeviceIDAsync, AdMobInterstitial } from "expo-ads-admob";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function Configuration() {
-  async function chargeAds(){
-    await setTestDeviceIDAsync('EMULATOR');
-    await AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/8691691433'); // Test ID, Replace with your-admob-unit-id
-    await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true});
-    await AdMobInterstitial.showAdAsync();
+export default function Configuration({navigation}) {
+  async function logout(){
+    await AsyncStorage.removeItem("User")
+    navigation.navigate("Login")
   }
-  chargeAds()  
-    
+
   return (
     <View style={tw`h-full bg-gray-900 flex justify-center items-center`}>
-      
+      <TouchableOpacity 
+        style={tw`w-40 h-10 bg-red-500 flex items-center justify-center rounded-md`}
+        onPress={()=>{logout()}} 
+      >
+        <Text style={tw`text-white text-center font-bold`}>LOG OUT</Text>
+      </TouchableOpacity>
     </View>
   );
 }
