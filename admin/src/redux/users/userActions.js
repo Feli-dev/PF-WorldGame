bc29805a9ebec40b248544e0efc4893cb6b98157
fern1s <101743969+fern1s@users.noreply.github.com>
2022-07-28 12:17:26 -0300

    Update userActions.js

--- admin/src/redux/users/userActions.js
+++ admin/src/redux/users/userActions.js
@@ -1,42 +1,46 @@
-import clienteAxios from '../../config/axios'
+import clienteAxios from "../../config/axios";
 
+import { GET_ALL_USERS } from "../../types";
 
-import { GET_ALL_USERS } from '../../types' 
-
-export function getAllUsers(){
-    
-    return async function(dispatch){
-        let allUsers = (await clienteAxios.get('/User/')).data
-        // console.log('Action', allUsers)
-            dispatch({
-            type: GET_ALL_USERS,
-            payload: allUsers
-        })
-    }
+export function getAllUsers() {
+  return async function (dispatch) {
+    let allUsers = (await clienteAxios.get("/User/")).data;
+    // console.log('Action', allUsers)
+    dispatch({
+      type: GET_ALL_USERS,
+      payload: allUsers,
+    });
+  };
 }
 
-
-export function deactivateUser(id){
-    return async function(){ 
+export function deactivateUser(id) {
+  return async function () {
     //console.log("ENTRÓ A ACCION")
-    let accions = await clienteAxios.delete(`/user/recycle/${id}`)
+    let accions = await clienteAxios.delete(`/user/recycle/${id}`);
     //console.log("ACCION DESACTIVADORA:", accions.data)
-    return accions.data
-    }
+    return accions.data;
+  };
 }
 
-export function reactivateUser(id){
-    return async function(){
-        let deact = await clienteAxios.delete(`/user/restore/${id}`)
-        return deact.data
-    }
+export function reactivateUser(id) {
+  return async function () {
+    let deact = await clienteAxios.delete(`/user/restore/${id}`);
+    return deact.data;
+  };
 }
 
-export function postUser (data){
-    return function(dispatch){
-        let created = clienteAxios.post('/User/', data)
-        console.log(created)
-        return created 
-    }
+export function postUser(data) {
+  return function (dispatch) {
+    let created = clienteAxios.post("/User/", data);
+    console.log(created);
+    return created;
+  };
+}
+
+export function getUser(id) {
+  return async function () {
+    let { data } = await clienteAxios.get(`/User/${id}`);
+    return data.Request;
+  };
 }
 
