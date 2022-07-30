ebb8ea4a4370f466a2a4ff2373923c762f0707a1
Emmanuel Avila <99704844+EmmAvila@users.noreply.github.com>
2022-07-29 21:15:34 -0500

    Update CardsDashboard.jsx

--- admin/src/components/CardsDashboard.jsx
+++ admin/src/components/CardsDashboard.jsx
@@ -9,6 +9,7 @@ import { useSelector } from "react-redux";
 
 const CardsDashboard = () => {
   let allUsers = useSelector(state => state.userReducer.users)
+  let payments = useSelector(state => state.payReducer.pays)
   return (
     <div className="grid grid-cols-4 gap-6 m-4">
       <div className="p-5  flex flex-col bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
@@ -28,7 +29,7 @@ const CardsDashboard = () => {
           <PaymentIcon />
         </div>
         <div className="mt-8">
-          <h3 className="text-2xl">0</h3>
+          <h3 className="text-2xl">{payments.length}</h3>
           <p className="text-gray-400">Confirmed</p>
         </div>
       </div>
