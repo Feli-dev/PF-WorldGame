1fb20283424127d591a73eacba70cc07a2b3a6d5
Qrow <45204415+SGL-2022@users.noreply.github.com>
2022-08-02 10:14:12 -0500

    User v4.3
    Mostrar usuarios con reviews y payment

--- api/src/Tools/filterShow.js
+++ api/src/Tools/filterShow.js
@@ -16,9 +16,12 @@ module.exports = {
             authorization: user.authorization,
             avatar: user.avatar,
             friends: user.Friends,
+            reviews: user. reviews,
+            payment: user.payment,
             games: user.games,
             stats: averageScore,
-            createdAt: user.createdAt
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt
         };
     }
 }
