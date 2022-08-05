f94c303d30a24a6703c74cf62f399eb7dd8423e0
Qrow <45204415+SGL-2022@users.noreply.github.com>
2022-08-05 10:19:40 -0500

    validacion country

--- api/src/controllers/country.js
+++ api/src/controllers/country.js
@@ -4,18 +4,21 @@ const { Country } = require("../db");
 async function getCountries() {
   let apiCountries = await axios.get("https://restcountries.com/v3/all");
   // console.log(apiCountries.data[0])
-  let formatedCountries = apiCountries.data.map((country) => {
-    return {
-      name: country.translations.spa.common,
-      continent: country.continents[0],
-      population: country.population,
-      latitud: country.latlng[0],
-      longitud: country.latlng[1],
-      googleMap: country.maps.googleMaps,
-      area: country.area,
-      flagSvg: country.flags[0],
-    };
-  });
+  let formatedCountries
+  if(apiCountries.length){
+    formatedCountries = apiCountries.data.map((country) => {
+      return {
+        name: country.translations.spa.common,
+        continent: country.continents[0],
+        population: country.population,
+        latitud: country.latlng[0],
+        longitud: country.latlng[1],
+        googleMap: country.maps.googleMaps,
+        area: country.area,
+        flagSvg: country.flags[0],
+      };
+    });
+  }
 
   let full = await Country.findAll()
   
