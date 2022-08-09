e538a239915c822a0dbc5567165f427c49bedaae
rafacarreno <89260514+rafacarreno@users.noreply.github.com>
2022-08-08 21:01:24 -0300

    upload FixSearch
    Se volvio a una version anterior para realizar cambios de diseño

--- client/views/Profile/Friend/SearchFriends.jsx
+++ client/views/Profile/Friend/SearchFriends.jsx
@@ -5,23 +5,16 @@ import {
   ToastAndroid,
   TextInput,
   Image,
-  Keyboard,
-  TouchableWithoutFeedback,
   ScrollView,
 } from "react-native";
 import avatarDefault from '../../../assets/avatar_default.png';
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { searchFriend, addFriend, GetProfileUser } from "../../../redux/actions/index";
-import { useNavigation } from '@react-navigation/native';
 import tw from "twrnc";
-import { FriendProfileBody } from "./FriendProfileBody";
-import BottomTabViewFriend from "./BottomTabViewFriend";
 import Ionic from 'react-native-vector-icons/Ionicons'
-import * as Animatable from 'react-native-animatable';
-import World from '../../../assets/World2.png';
-import Loading from '../../../assets/loading.gif';
-
+//import * as Animatable from 'react-native-animatable';
+//import World from '../../../assets/World2.png';
 
 //let lengthOfObject = Object.keys(obj).lengt
 //<BottomTabView />
@@ -40,7 +33,7 @@ const SearchFriends = (params) => {
     refresher: ""
   });
 
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const [find, setFind] = useState(false);
 
 
@@ -61,7 +54,7 @@ const SearchFriends = (params) => {
     dispatch(GetProfileUser(id))
     dispatch(searchFriend(userData.username));
     setFind(true);
-    setTimeout(() => setOpen(false), 1200)
+    setTimeout(() => setOpen(true), 1200)
     SetUserData({
       ...userData,
       username: "",
@@ -99,7 +92,8 @@ const SearchFriends = (params) => {
 
 
 
-  return (//findUsers.length > 0 && open ? 
+
+  return findUsers.length > 0 ? (
     <ScrollView
       style={{
         backgroundColor: '#111827',
@@ -146,167 +140,108 @@ const SearchFriends = (params) => {
           />
         </View>
       </View>
+      {findUsers.map((user) => {
+        if (user.id !== id) return (
+          <TouchableOpacity
+            key={user.id}
+            onPress={() => navigation.navigate('FriendProfile', { freId: user.id, userId: id, userFriends: friends })}
+          >
 
-{/* 
+            <View style={{ width: "100%", marginBottom: 15, marginHorizontal: 20, backgroundColor: '#111827' }}
 
-find -> true -> che el arreglo de usuarios esta vacio? -si / no
-        
-NO MEZCLAR CON LAS COMPROBACIONES. DIFERENCIAR ENTRE TOCAR EL BOTON Y NO Y LUEGO 
-
-      false -> false
-
-
-
-
-*/}
-
-
-
-      {findUsers.length > 0 && !open
-        ? (findUsers.map((user) => {
-          if (user.id !== id) return (
-            <TouchableOpacity
-              key={user.id}
-              onPress={() => navigation.navigate('FriendProfile', { freId: user.id, userId: id, userFriends: friends })}
+            // onPress={() =>
+            //   navigation.navigate("FriendProfile", { freId: friend.FriendId })
+            // }
             >
-
-              <View style={{ width: "100%", marginBottom: 15, marginHorizontal: 20, backgroundColor: '#111827' }}
-
+              <View
+                style={{ width: "100%", marginBottom: 15, }}
               >
                 <View
-                  style={{ width: "100%", marginBottom: 15, }}
+                  style={{
+                    borderRadius: 8, backgroundColor: "grey", padding: 15,
+                    justifyContent: "space-between", alignItems: "center", display: "flex",
+                    flexDirection: "row", marginHorizontal: 10, width: '90%'
+                  }}
                 >
-                  <View
+                  <Image
                     style={{
-                      borderRadius: 8, backgroundColor: "grey", padding: 15,
-                      justifyContent: "space-between", alignItems: "center", display: "flex",
-                      flexDirection: "row", marginHorizontal: 10, width: '90%', paddingHorizontal: 30,
+                      width: 50,
+                      height: 50,
                     }}
+                    source={user.avatar ? user.avatar : avatarDefault}
+                  />
+
+                  <Text style={{
+                    color: "white", fontSize: 25, textAlign: "center", paddingRight: 10,
+                  }}
                   >
-                    <Image
-                      source={{ uri: user.avatar }}
+                    {user.username ? user.username : "Unknow"}
+
+                  </Text>
+                  {friends.some(friend => friend.FriendId === user.id)
+                    ? <View><Text>        </Text></View>
+                    : <Ionic
+                      onPress={() => handleAdd({ UserId: id, FriendId: user.id })}
+                      name="person-add"
                       style={{
-                        width: 50,
-                        height: 50,
+                        fontSize: 30,
+                        color: 'white',
                       }}
                     />
-                    <Text style={{
-                      color: "white", fontSize: 25, textAlign: "center", paddingRight: 10,
-                    }}
-                    >
-                      {user.username ? user.username : "Unknow"}
+                  }
 
-                    </Text>
-
-                    {friends.some(friend => friend.FriendId === user.id)
-                      ? <View><Text>        </Text></View>
-                      : <Ionic
-                        onPress={() => handleAdd({ UserId: id, FriendId: user.id })}
-                        name="person-add"
-                        style={{
-                          fontSize: 30,
-                          color: 'white',
-                        }}
-                      />
-                    }
-
-                  </View>
                 </View>
               </View>
-            </TouchableOpacity>
-          )
-        }))
-        :
-        find && findUsers.length === 0
-          ? (
-            <View style={{
-              marginTop: 100,
-              display: 'flex',
-              alignContent: 'center',
-              justifyContent: 'center',
+            </View>
+          </TouchableOpacity>
+        );
+      })}
+    </ScrollView>
+  ) : (
+    <View style={{ backgroundColor: '#111827', height: '100%' }}>
 
-            }}>
-              <Text style={{ color: "#D1D5DB", fontSize: 30, padding: 5, textAlign: 'center' }}>
-                No results...   😥
-              </Text>
-              <Animatable.View
-                animation="fadeInLeftBig" iterationCount={1} duration={3000}
-                style={{
-                  marginTop: 30,
-                  paddingHorizontal: 45,
-                }}
-              >
-                <Image
-                  source={World}
-                />
-              </Animatable.View>
-            </View>)
-
-          : find && open?
-          <View>
-            <Image
-            source={Loading}
+      <View style={{ padding: 50, }}>
+        <View>
+          <View
             style={{
-              width:90,
-              height:90,
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+
             }}
+          >
+            <Text style={{ opacity: 0.5, color: "#D1D5DB", fontSize: 20, padding: 5, }}>
+              Find a friend
+            </Text>
+            <Ionic
+              onPress={() => handleSearch()}
+              name="search"
+              style={{
+                fontSize: 30,
+                color: '#D1D5DB',
+              }}
             />
           </View>
-
-          :<View><Text></Text></View>
-
-      }
-
-
-    </ScrollView>
-  )
-  // : (  find
-  //   <View style={{ backgroundColor: '#111827', height: '100%' }}>
-
-  //     <View style={{ padding: 50, }}>
-  //       <View>
-  //         <View
-  //           style={{
-  //             display: 'flex',
-  //             flexDirection: 'row',
-  //             justifyContent: 'space-between',
-
-  //           }}
-  //         >
-  //           <Text style={{ opacity: 0.5, color: "#D1D5DB", fontSize: 20, padding: 5, }}>
-  //             Find a friend
-  //           </Text>
-  //           <Ionic
-  //             onPress={() => handleSearch()}
-  //             name="search"
-  //             style={{
-  //               fontSize: 30,
-  //               color: '#D1D5DB',
-  //             }}
-  //           />
-  //         </View>
-  //         <TextInput
-  //           type="username"
-  //           placeholder="Search username"
-  //           placeholderTextColor="#6B7280"
-  //           defaultValue={userData.username}
-  //           style={{
-  //             fontSize: 25,
-  //             borderBottomWidth: 1,
-  //             borderColor: "#CDCDCD",
-  //             color: "#D1D5DB",
-  //             paddingTop: 15,
-  //             paddingBottom: 5,
-  //           }}
-  //           onChangeText={(event) => handleOnChange("username", event)}
-  //         />
-
-
-
-  //       </View>
-  //     </View>
-  //   </View>
-  // );
+          <TextInput
+            type="username"
+            placeholder="Search username"
+            placeholderTextColor="#6B7280"
+            defaultValue={userData.username}
+            style={{
+              fontSize: 25,
+              borderBottomWidth: 1,
+              borderColor: "#CDCDCD",
+              color: "#D1D5DB",
+              paddingTop: 15,
+              paddingBottom: 5,
+            }}
+            onChangeText={(event) => handleOnChange("username", event)}
+          />
+        </View>
+      </View>
+    </View>
+  );
 };
+
 
 export default SearchFriends;
